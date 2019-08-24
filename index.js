95785e4b365332f5c27fd4229d35e446f01b7a5b
yash desai <yhdesai99@ymail.com>
2019-08-24 13:51:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
     return newObj
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
 object[key] = null
 return object
 
