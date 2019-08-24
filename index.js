99e59907cbd63631abd41b7ab417b10c8f3e31f7
yash desai <yhdesai99@ymail.com>
2019-08-24 13:50:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = Object.assign({}, null)
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object)
+newObj[key] = value
+return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+object[key] = value
+
+return object
+}
+function deleteFromObjectByKey(object, key) {
+    var newObj = Object.assign({}, object)
+    newObj[key] = null
+    return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key) {
+object[key] = null
+return object
+
+}
